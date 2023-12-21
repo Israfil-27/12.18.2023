2d6b60a43a0555718e3d1e45e20f5fc9e15297fe
Israfil666 <113192535+Israfil666@users.noreply.github.com>
2023-12-21 02:04:50 -0800

    cards

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 function buildStyles() {
-    return src('index.scss')
+    return src('sass/index.scss')
         .pipe(sass())
         .pipe(dest('css'));
 }
 function watchTask() {
-    watch(['index.scss'], buildStyles);
+    watch(['sass/index.scss'], buildStyles);
 }
 exports.default = series(buildStyles, watchTask);
